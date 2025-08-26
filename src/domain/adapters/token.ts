a478e0be85e4dbcc2b0e7dc092d254992d7b181c
JPSouzaSilva <jpsouzadasilva@gmail.com>
2025-08-26 20:40:00 -0300

    feat: login route

---
+++ src/domain/adapters/token.ts
@@ -0,0 +1,20 @@
+import { UserRole } from "@domain/entities/user-role-enum";
+
+export interface TokenPayload {
+  id: string;
+  email: string;
+  role: UserRole;
+}
+
+export interface GetPayloadFromTokenOutput {
+  id: string;
+  role: UserRole;
+  exp: number;
+}
+
+export abstract class TokenAdapter {
+  abstract generateToken(payload: TokenPayload): Promise<string>;
+  abstract getPayloadFromToken(
+    token: string
+  ): Promise<GetPayloadFromTokenOutput>;
+}
