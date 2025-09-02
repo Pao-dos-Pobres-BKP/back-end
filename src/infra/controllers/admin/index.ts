3ceb9f98047e592fc2f7ed7efe8002a2a75438b7
Luca Partichelli Mandelli <luca.partichelli@edu.pucrs.br>
2025-09-02 15:37:33 -0300

    feat: add tests for admin crud

--- src/infra/controllers/admin/index.ts
+++ src/infra/controllers/admin/index.ts
@@ -18,7 +18,7 @@ import {
 import { PaginationDTO } from "@application/dtos/utils/pagination";
 import { CreateAdminUseCase } from "@application/use-cases/admin/create/create-admin";
 import { DeleteAdminUseCase } from "@application/use-cases/admin/delete/delete-admin";
-import { FindAllAdminsUseCase } from "@application/use-cases/admin/find-all/find-all-admins.";
+import { FindAllAdminsUseCase } from "@application/use-cases/admin/find-all/find-all-admins";
 import { FindAdminByIdUseCase } from "@application/use-cases/admin/find-by-id/find-admin-by-id";
 import { UpdateAdminUseCase } from "@application/use-cases/admin/update/update-admin";
 import {
--- src/infra/modules/admin/index.ts
+++ src/infra/modules/admin/index.ts
@@ -7,7 +7,7 @@ import { FindAdminByIdUseCase } from "@application/use-cases/admin/find-by-id/fi
 import { CreateAdminUseCase } from "@application/use-cases/admin/create/create-admin";
 import { UpdateAdminUseCase } from "@application/use-cases/admin/update/update-admin";
 import { DeleteAdminUseCase } from "@application/use-cases/admin/delete/delete-admin";
-import { FindAllAdminsUseCase } from "@application/use-cases/admin/find-all/find-all-admins.";
+import { FindAllAdminsUseCase } from "@application/use-cases/admin/find-all/find-all-admins";
 
 @Module({
   imports: [DatabaseModule, ExceptionModule, HashModule],
