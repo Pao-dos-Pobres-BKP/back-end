4e140af78855ef3d36f72121bb8b2ad1b652142e
Luiz <luizmoraesaf@gmail.com>
2025-09-10 17:49:34 -0300

    feat: add s3 integration to add, remove and find files

---
+++ src/application/use-cases/s3storage/create/create-file.ts
@@ -0,0 +1,39 @@
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import {
+  CreateFileDTO,
+  CreateFileResponse
+} from "@application/dtos/s3storage/create";
+import { S3IntegrationHelper } from "@infra/integrations/s3";
+
+export class CreateFileUseCase {
+  constructor(
+    private readonly s3: S3Client,
+    private readonly s3Helper: S3IntegrationHelper
+  ) {}
+
+  async execute(file: CreateFileDTO): Promise<CreateFileResponse> {
+    const Key = this.s3Helper.buildKey(file.originalname);
+    await this.s3.send(
+      new PutObjectCommand({
+        Bucket: this.s3Helper.bucket(),
+        Key,
+        Body: file.buffer,
+        ContentType: file.mimetype
+      })
+    );
+    await this.s3.send(
+      new PutObjectCommand({
+        Bucket: this.s3Helper.bucket(),
+        Key,
+        Body: file.buffer,
+        ContentType: file.mimetype || "application/octet-stream"
+      })
+    );
+
+    return {
+      key: Key,
+      contentType: file.mimetype || "application/octet-stream",
+      size: file.buffer.length
+    };
+  }
+}
