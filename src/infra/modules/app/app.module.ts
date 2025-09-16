fff86128a1f6de407037bd040aca7bda39a94f54
VitoriaFrohlich <vitoriafrohlichh@gmail.com>
2025-09-15 22:43:38 -0300

    feat: add enum Periodicity in donation

--- src/infra/modules/app/app.module.ts
+++ src/infra/modules/app/app.module.ts
@@ -1,7 +1,9 @@
 import { EnvConfig } from "@infra/config/env";
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+
 import { DonorModule } from "../donor";
+import { DonationModule } from "../donation";
 
 @Module({
   imports: [
@@ -9,7 +11,8 @@ import { DonorModule } from "../donor";
       isGlobal: true,
       load: [() => EnvConfig.validate(process.env)]
     }),
-    DonorModule
+  DonorModule,
+  DonationModule
   ]
 })
 export class AppModule {}
--- src/infra/modules/database/index.ts
+++ src/infra/modules/database/index.ts
@@ -2,15 +2,21 @@ import { DonorRepository } from "@domain/repositories/donor";
 import { PrismaService } from "@infra/config/prisma";
 import { PrismaDonorRepository } from "@infra/repositories/prisma/donor";
 import { Module } from "@nestjs/common";
+import { DonationRepository } from "@domain/repositories/donation";
+import { PrismaDonationRepository } from "@infra/repositories/prisma/donation";
 
 @Module({
   providers: [
     PrismaService,
     {
-      useClass: PrismaDonorRepository,
-      provide: DonorRepository
+      provide: DonorRepository,
+      useClass: PrismaDonorRepository
+    },
+    {
+      provide: DonationRepository,
+      useClass: PrismaDonationRepository
     }
   ],
-  exports: [DonorRepository]
+  exports: [DonorRepository, DonationRepository]
 })
 export class DatabaseModule {}
