9d3adfd8616249e1c50803c025b710da6f769102
Luca Partichelli Mandelli <luca.partichelli@edu.pucrs.br>
2025-09-20 19:22:11 -0300

    add tests for campaigns crud

---
+++ test/stubs/repositories/campaign.ts
@@ -0,0 +1,34 @@
+import { PaginatedEntity } from "@domain/constants/pagination";
+import { Campaign } from "@prisma/client";
+import {
+  CampaignRepository,
+  CampaignDetailsResponse
+} from "@domain/repositories/campaign";
+
+export class CampaignRepositoryStub implements CampaignRepository {
+  async create(): Promise<void> {
+    return;
+  }
+
+  async findById(): Promise<Campaign | null> {
+    return;
+  }
+
+  async findByTitleAndDateAndStatus(): Promise<
+    PaginatedEntity<CampaignDetailsResponse>
+  > {
+    return;
+  }
+
+  async findAll(): Promise<PaginatedEntity<CampaignDetailsResponse>> {
+    return;
+  }
+
+  async update(): Promise<void> {
+    return;
+  }
+
+  async delete(): Promise<void> {
+    return;
+  }
+}
