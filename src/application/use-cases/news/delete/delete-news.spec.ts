bd2bd0961b55ccdfa19edf6d7224a97df685e89a
Salette Costa <salette.costa@edu.pucrs.br>
2025-09-20 15:11:22 -0300

    fix: adjust news tests

--- src/application/use-cases/news/delete/delete-news.spec.ts
+++ src/application/use-cases/news/delete/delete-news.spec.ts
@@ -1,12 +1,11 @@
 import { DeleteNewsUseCase } from "./delete-news";
 import { NewsRepository } from "@domain/repositories/news";
-import { NotFoundException } from "@nestjs/common";
 import { News } from "@domain/entities/news";
-import { ExceptionsAdapter } from "@domain/adapters/exception";
+import { ExceptionsServiceStub } from "@test/stubs/adapters/exceptions";
 
 describe("DeleteNewsUseCase", () => {
   let repo: jest.Mocked<NewsRepository>;
-  let exceptions: jest.Mocked<ExceptionsAdapter>;
+  let exceptions: ExceptionsServiceStub;
   let useCase: DeleteNewsUseCase;
 
   beforeEach(() => {
@@ -18,32 +17,29 @@ describe("DeleteNewsUseCase", () => {
       update: jest.fn()
     } as unknown as jest.Mocked<NewsRepository>;
 
-    exceptions = {
-      badRequest: jest.fn(),
-      conflict: jest.fn(),
-      internalServerError: jest.fn(),
-      forbidden: jest.fn(),
-      unauthorized: jest.fn(),
-      notFound: jest.fn()
-    };
-
+    exceptions = new ExceptionsServiceStub();
     useCase = new DeleteNewsUseCase(repo, exceptions);
+
+    jest.spyOn(exceptions, "notFound");
   });
 
   it("should delete an existing news", async () => {
     repo.findById.mockResolvedValueOnce({ id: "valid-id" } as unknown as News);
     repo.delete.mockResolvedValueOnce();
 
-    await expect(useCase.execute("valid-id")).resolves.toBeUndefined();
+    await useCase.execute("valid-id");
+
     expect(repo.delete).toHaveBeenCalledWith("valid-id");
   });
 
-  it("should throw NotFoundException if the news does not exist", async () => {
+  it("should call exceptions.notFound when the news does not exist", async () => {
     repo.findById.mockResolvedValueOnce(null);
 
-    await expect(useCase.execute("invalid-id")).rejects.toThrow(
-      NotFoundException
-    );
-    expect(exceptions.notFound).toHaveBeenCalledWith("News not found");
+    await useCase.execute("invalid-id");
+
+    expect(exceptions.notFound).toHaveBeenCalledWith({
+      message: "News not found"
+    });
+    expect(repo.delete).not.toHaveBeenCalled();
   });
 });
