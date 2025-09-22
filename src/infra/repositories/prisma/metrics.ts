a8c2e3dc5754583d9f32fdffe68ed0578392f72b
fredericonizoli <fredericonizoli@gmail.com>
2025-09-21 21:18:26 -0300

    Correcting SQL query on metrics repository

--- src/infra/repositories/prisma/metrics.ts
+++ src/infra/repositories/prisma/metrics.ts
@@ -17,18 +17,25 @@ export class MetricsRepository {
     };
 
     const result = await this.prisma.$queryRawUnsafe<RawMetricsQueryResult[]>(`
-  SELECT 
-    COALESCE(SUM(d."amount"), 0) AS total_raised,
-    COUNT(DISTINCT CASE WHEN d."is_recurring" THEN d."id" END) AS recurring_donations,
-    COUNT(*) AS total_donations,
-    COALESCE(AVG(d."amount"), 0) AS average_ticket,
-    (
-      SELECT COUNT(*) FROM "Donor"
-      WHERE "createdAt" >= NOW() - INTERVAL '${days} days'
-    ) AS new_donors
-  FROM "Donation" d
-  WHERE d."createdAt" >= NOW() - INTERVAL '${days} days'
-`);
+      SELECT 
+        COALESCE(SUM(d."amount"), 0) AS total_raised,
+        COUNT(DISTINCT CASE WHEN d."is_recurring" THEN d."id" END) AS recurring_donations,
+        COUNT(*) AS total_donations,
+        COALESCE(AVG(d."amount"), 0) AS average_ticket,
+        (
+          SELECT COUNT(*) 
+          FROM "Donor" don
+          JOIN "Donation" d2 ON don.id = d2."donorId"
+          JOIN "Payment" p ON d2.id = p."donationId"
+          WHERE don."createdAt" >= NOW() - INTERVAL '${days} days'
+            AND d2."createdAt" >= NOW() - INTERVAL '${days} days'
+            AND p."status" IN ('CONFIRMED', 'AUTHORIZED')
+        ) AS new_donors
+      FROM "Donation" d
+      JOIN "Payment" p ON d.id = p."donationId"
+      WHERE d."createdAt" >= NOW() - INTERVAL '${days} days'
+        AND p."status" IN ('CONFIRMED', 'AUTHORIZED')
+    `);
 
     const raw = result[0];
     return MetricsMapper.toPeriodMetrics(raw);
