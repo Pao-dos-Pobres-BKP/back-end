5660024aec5b32151c68d8c6738b1a82fdd4fc7b
Gabriel Camargo <gcamargo.dev@gmail.com>
2025-09-27 17:54:19 -0300

    feat: create newsletter useCase

---
+++ src/application/use-cases/newsletter/create/create-newsletter.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@nestjs/common";
+import { ExceptionsAdapter } from "@domain/adapters/exception";
+import { NewsletterRepository } from "@domain/repositories/newsletter";
+import {
+  CreateNewsletterDto,
+  CreateNewsletterResponse
+} from "@application/dtos/newsletter/create";
+
+@Injectable()
+export class CreateNewsletterUseCase {
+  constructor(
+    private readonly newsletterRepository: NewsletterRepository,
+    private readonly exception: ExceptionsAdapter
+  ) {}
+
+  async execute(dto: CreateNewsletterDto): Promise<CreateNewsletterResponse> {
+    const { email } = dto;
+
+    const existingSubscription =
+      await this.newsletterRepository.findByEmail(email);
+
+    if (existingSubscription) {
+      this.exception.badRequest({
+        message: "Email já inscrito na newsletter"
+      });
+    }
+
+    await this.newsletterRepository.create({ email });
+
+    return {
+      message: "Inscrição na newsletter realizada com sucesso"
+    };
+  }
+}
