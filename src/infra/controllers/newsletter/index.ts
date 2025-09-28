62d279c6ad287cc13992887350d770cd5809bf54
Gabriel Camargo <gcamargo.dev@gmail.com>
2025-09-28 20:41:01 -0300

    feat: create export excel endpoint

--- src/infra/controllers/newsletter/index.ts
+++ src/infra/controllers/newsletter/index.ts
@@ -1,16 +1,27 @@
-import { Body, Controller, Post, HttpCode, HttpStatus } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Post,
+  HttpCode,
+  HttpStatus,
+  Get,
+  Res,
+  Header
+} from "@nestjs/common";
 import { ApiTags, ApiOperation, ApiResponse } from "@nestjs/swagger";
 import {
   CreateNewsletterDto,
   CreateNewsletterResponse
 } from "@application/dtos/newsletter/create";
 import { CreateNewsletterUseCase } from "@application/use-cases/newsletter/create/create-newsletter";
-
+import { ExportExcelUseCase } from "@application/use-cases/newsletter/export-excel/export-excel";
+import { Response } from "express";
 @ApiTags("Newsletter")
 @Controller("newsletter")
 export class NewsletterController {
   constructor(
-    private readonly createNewsletterUseCase: CreateNewsletterUseCase
+    private readonly createNewsletterUseCase: CreateNewsletterUseCase,
+    private readonly exportExcelUseCase: ExportExcelUseCase
   ) {}
 
   @Post()
@@ -28,5 +39,22 @@ export class NewsletterController {
     @Body() body: CreateNewsletterDto
   ): Promise<CreateNewsletterResponse> {
     return await this.createNewsletterUseCase.execute(body);
+  }
+
+  @Get()
+  @ApiOperation({
+    summary: "Exportar inscrições na newsletter em .xlsx (Excel)"
+  })
+  @Header(
+    "Content-Type",
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+  )
+  @Header(
+    "Content-Disposition",
+    'attachment; filename="inscricoes-newsletter.xlsx"'
+  )
+  async exportSubscriptions(@Res() response: Response): Promise<void> {
+    const excelBuffer = await this.exportExcelUseCase.execute();
+    response.send(excelBuffer);
   }
 }
