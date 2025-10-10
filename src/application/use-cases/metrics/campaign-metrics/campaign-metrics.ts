5e163566db4aa113e4e105c7996e98bb86978ac1
Gabriel Camargo <gcamargo.dev@gmail.com>
2025-10-10 18:35:18 -0300

    refactor: refactor campaign metrics

--- src/application/use-cases/metrics/campaign-metrics/campaign-metrics.ts
+++ src/application/use-cases/metrics/campaign-metrics/campaign-metrics.ts
@@ -1,10 +1,7 @@
 import { ExceptionsAdapter } from "@domain/adapters/exception";
 import { CampaignRepository } from "@domain/repositories/campaign";
 import { Injectable } from "@nestjs/common";
-import {
-  DonationDetailsResponseWithPayment,
-  DonationRepository
-} from "@domain/repositories/donation";
+import { DonationRepository } from "@domain/repositories/donation";
 import { PaymentMethod, PaymentStatus } from "@prisma/client";
 import { Campaing } from "@domain/entities/campaign";
 
@@ -27,67 +24,60 @@ export class CampaignMetricsUseCase {
   ) {}
 
   async execute(id: string): Promise<CampaignWithMetrics | void> {
-    try {
-      const campaign = await this.campaignRepository.findById(id);
-      if (!campaign) {
-        return this.exceptionService.notFound({
-          message: "Campaign not found"
-        });
-      }
+    const campaign = await this.campaignRepository.findById(id);
+    if (!campaign) {
+      return this.exceptionService.notFound({
+        message: "Campaign not found"
+      });
+    }
 
-      const donations = await this.donationRepository.findAllByCampaign(id);
+    const donations = await this.donationRepository.findAllByCampaign(id);
 
-      const donationsWithConfirmedPayments = donations.map((donation) => ({
-        ...donation,
-        payment: donation.payment.filter(
-          (payment) => payment.status === PaymentStatus.CONFIRMED
-        )
-      }));
+    const donationsWithConfirmedPayments = donations.map((donation) => ({
+      ...donation,
+      payment: donation.payment.filter(
+        (payment) => payment.status === PaymentStatus.CONFIRMED
+      )
+    }));
 
-      const donationsGroupedByPaymentMethod = donationsWithConfirmedPayments
-        .filter((donation) => donation.payment.length > 0)
-        .reduce(
-          (acc, donation) => {
-            const paymentMethod = donation.payment[0].paymentMethod;
-            if (!acc[paymentMethod]) {
-              acc[paymentMethod] = [];
-            }
-            acc[paymentMethod].push(donation);
-            return acc;
-          },
-          {} as Record<PaymentMethod, DonationDetailsResponseWithPayment[]>
+    const donationsGroupedByPaymentMethod = donationsWithConfirmedPayments
+      .filter((donation) => donation.payment.length > 0)
+      .reduce((acc, donation) => {
+        const paymentMethod = donation.payment[0].paymentMethod;
+        if (!acc[paymentMethod]) {
+          acc[paymentMethod] = [];
+        }
+        acc[paymentMethod].push(donation);
+        return acc;
+      }, {});
+
+    const paymentMethods = Object.keys(donationsGroupedByPaymentMethod);
+
+    const paymentMethodsWithMetrics: PaymentComparison[] = paymentMethods.map(
+      (paymentMethod) => {
+        const donations = donationsGroupedByPaymentMethod[paymentMethod];
+
+        const totalAmount: number = donations.reduce(
+          (acc, donation) => acc + donation.amount,
+          0
         );
 
-      const paymentMethods = Object.keys(donationsGroupedByPaymentMethod);
+        const totalCount: number = donations.length;
+        return {
+          paymentMethod: paymentMethod as PaymentMethod,
+          totalAmount,
+          totalCount
+        };
+      }
+    );
 
-      const paymentMethodsWithMetrics: PaymentComparison[] = paymentMethods.map(
-        (paymentMethod) => {
-          const donations = donationsGroupedByPaymentMethod[paymentMethod];
+    const campaignWithMetrics = {
+      ...campaign,
+      paymentComparison: paymentMethodsWithMetrics,
+      targetAmount: campaign.targetAmount.toNumber(),
+      currentAmount: campaign.currentAmount.toNumber()
+    };
 
-          const totalAmount: number = donations.reduce(
-            (acc, donation) => acc + donation.amount,
-            0
-          );
-
-          const totalCount: number = donations.length;
-          return {
-            paymentMethod: paymentMethod as PaymentMethod,
-            totalAmount,
-            totalCount
-          };
-        }
-      );
-
-      const campaignWithMetrics = {
-        ...campaign,
-        paymentComparison: paymentMethodsWithMetrics,
-        targetAmount: campaign.targetAmount.toNumber(),
-        currentAmount: campaign.currentAmount.toNumber()
-      } as CampaignWithMetrics;
-
-      return campaignWithMetrics;
-    } catch (error) {
-      throw error;
-    }
+    return campaignWithMetrics;
   }
 }
