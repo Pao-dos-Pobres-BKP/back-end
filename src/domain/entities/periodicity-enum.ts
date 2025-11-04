55bd4afbab4774db167d5448424b1eb34b15b4ce
JPSouzaSilva <jpsouzadasilva@gmail.com>
2025-11-04 18:12:57 -0300

    add canceled in donation periodicity

--- src/domain/entities/periodicity-enum.ts
+++ src/domain/entities/periodicity-enum.ts
@@ -2,7 +2,8 @@ export const Periodicity = {
   MONTHLY: "MONTHLY",
   QUARTERLY: "QUARTERLY",
   SEMIANNUALLY: "SEMI_ANNUAL",
-  YEARLY: "YEARLY"
+  YEARLY: "YEARLY",
+  CANCELED: "CANCELED"
 } as const;
 
 export type Periodicity = (typeof Periodicity)[keyof typeof Periodicity];
