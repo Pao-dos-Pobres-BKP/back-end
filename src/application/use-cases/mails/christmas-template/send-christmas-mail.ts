bc15edd28ff01a51fb4f2ed8980144e0c66d1b36
luis.rossi <rossi.luis@edu.pucrs.br>
2025-11-13 15:00:41 -0300

    remove console.log

--- src/application/use-cases/mails/christmas-template/send-christmas-mail.ts
+++ src/application/use-cases/mails/christmas-template/send-christmas-mail.ts
@@ -14,17 +14,13 @@ export class SendChristmasMailUseCase {
     const donors = await this.donorRepository.findAllDonorsMails();
 
     const title = "Feliz Natal";
-    console.log("Preparing to send Christmas mails to donors:", donors.length);
 
     for (const donor of donors) {
-      console.log("Sending Christmas mail to:", donor.email);
       await this.queueIntegration.addJob({
         to: donor.email,
         subject: title,
         body: christmasTemplate(title, donor.fullName)
       });
-      console.log("Christmas mail queued for:", donor.email);
     }
-    console.log("Sending of Christmas mails completed.");
   }
 }
